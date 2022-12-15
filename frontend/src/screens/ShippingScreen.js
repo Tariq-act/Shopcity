fec9e7fd401d3754d0dd390332988a470f2aecc0
Tariq Khan <khantarikact@gmail.com>
2022-12-15 09:34:43 +0530

    Shipping screen and Payment screen and its functionality - 3

--- frontend/src/screens/ShippingScreen.js
+++ frontend/src/screens/ShippingScreen.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { useLocation, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { Form, Button } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
 import FormContainer from '../components/FormContainer';
