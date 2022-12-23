8a3dbbd2c1374376cbd5a85099923d441f9153ee
Tariq Khan <khantarikact@gmail.com>
2022-12-23 09:14:15 +0530

    add functionality to write review and give rating

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -9,6 +9,7 @@ import {
   productDeleteReducer,
   productCreateReducer,
   productUpdateReducer,
+  productReviewCreateReducer,
 } from './reducers/productReducers';
 import { cartReducer } from './reducers/cartReducers';
 import {
@@ -35,6 +36,7 @@ const reducer = combineReducers({
   productDelete: productDeleteReducer,
   productCreate: productCreateReducer,
   productUpdate: productUpdateReducer,
+  productReviewCreate: productReviewCreateReducer,
   cart: cartReducer,
   userLogin: userLoginReducer,
   userRegister: userRegisterReducer,
