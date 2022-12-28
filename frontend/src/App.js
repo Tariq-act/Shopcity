3331809e8b5cfa215b4e242e3e72805f06ae3aef
Tariq Khan <khantarikact@gmail.com>
2022-12-28 14:28:05 +0530

    bug fix

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Container } from 'react-bootstrap';
+
 import Header from './components/Header';
 import Footer from './components/Footer';
 import HomeScreen from './screens/HomeScreen';
